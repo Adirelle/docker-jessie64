d20726ef085780d12711d2708d1cf7650a56c654
Adirelle <adirelle+github@gmail.com>
2016-02-21 11:19:48 +0100

    Create /etc/fish/completions before downloading docker completions.

--- scripts/fish.sh
+++ scripts/fish.sh
@@ -7,6 +7,7 @@ apt-get -y install fish
 
 echo "updating fish completions"
 
+mkdir -p /etc/fish/completions
 wget https://raw.github.com/barnybug/docker-fish-completion/master/docker.fish -O /etc/fish/completions/docker.fish
 
 su - vagrant -c "/usr/bin/fish -c fish_update_completions"
